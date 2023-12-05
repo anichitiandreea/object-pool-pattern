ce1a3ee17b0f5b85abc32d58bafb165ab5c6e6bd
Andreea Anichiti <andreea.anichiti@gmail.com>
2023-12-05 15:09:04 +0200

    Add new test for checking the pool size in case many objects are returned

--- tests/ObjectPoolTest.cs
+++ tests/ObjectPoolTest.cs
@@ -5,17 +5,17 @@ namespace Tests
     public class ObjectPoolTest
     {
         [Fact]
-        public void GivenGetObjectWhenPoolIsEmptyThenCheckCreatedObject()
+        public void GivenGetObjectWhenPoolIsEmptyThenCheckPoolCount()
         {
             // Arrange
             var objectPool = new ObjectPool<Student>(3);
 
             // Act
-            var result = objectPool.GetObject();
+            var object1 = objectPool.GetObject();
 
             // Assert
             Assert.Equal(0, objectPool.GetObjectCount());
-            Assert.NotNull(result);
+            Assert.NotNull(object1);
         }
 
         [Fact]
@@ -25,13 +25,32 @@ namespace Tests
             var objectPool = new ObjectPool<Student>(2);
 
             // Act
-            var result1 = objectPool.GetObject();
-            var result2 = objectPool.GetObject();
+            var object1 = objectPool.GetObject();
+            var object2 = objectPool.GetObject();
 
-            objectPool.ReturnObject(result2);
+            objectPool.ReturnObject(object2);
 
             // Assert
             Assert.Equal(1, objectPool.GetObjectCount());
+        }
+
+        [Fact]
+        public void GivenReturnObjectWhenManyObjectsReturnedThenCheckPoolCount()
+        {
+            // Arrange
+            var objectPool = new ObjectPool<Student>(2);
+
+            // Act
+            var object1 = objectPool.GetObject();
+            var object2 = objectPool.GetObject();
+            var object3 = objectPool.GetObject();
+
+            objectPool.ReturnObject(object1);
+            objectPool.ReturnObject(object2);
+            objectPool.ReturnObject(object3);
+
+            // Assert
+            Assert.Equal(2, objectPool.GetObjectCount());
         }
     }
 }
